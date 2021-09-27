42685b54bb4292033ee3d35ea560e7aa36840769
Goran Topic <amadanmath+github@gmail.com>
2021-09-27 11:47:06 +0900

    Rework `sign.sh`

--- bin/sign.sh
+++ bin/sign.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-here="$(dirname "$0")"
-source "$here/../venv/bin/activate"
-cd "$here/../app"
-python -m s3downloader.aws
+# crontab
+# 0	0	*	*	*	/path/to/s3downloader/bin/sign.sh /path/to/envfile
+
+envpath="$1"
+if [[ -z "$envpath" ]]
+then
+  echo "Usage: $0 <env_file>" > /dev/stderr
+  exit 1
+fi
+source "$envpath"
+
+top="$(cd "$(dirname "$0")"/..; pwd)"
+cd "$top/app"
+"$top/venv/bin/python" -m s3downloader.aws
