324da2f29c5d24236351cb5303cc46e650e802b8
Goran Topic <amadanmath+github@gmail.com>
2021-09-30 12:54:51 +0900

    Implement mail whitelist

--- README.md
+++ README.md
@@ -32,4 +32,28 @@ Name is obligatory, everything else is optional.
 
 `bin/sign.sh` can be used to pre-pre-sign all the corpora, so that URLs are
 not generated when the user requests them. It would be best if it was set up
-as a daily cron job.
+as a daily cron job. It should also be run if an URL list is added or changed.
+
+An email whitelist can be set up at `"$DATA_DIR/mail_whitelist.txt"`. If an
+email is whitelisted, the admin will not be asked for verification, and the
+links will be sent automatically upon request. An email can also be greylisted
+in the same file by prepending an exclamation mark. Patterns are listed one
+per line. Empty lines and lines starting with `#` are ignored. If an email
+matches both a whitelist pattern and a greylist pattern, the more specific one
+is applied, with preference to greylist ones in case of a tie. These are the
+possible patterns:
+
+```
+# a specific email address (only matches user@example.com)
+user@example.com
+
+# a specific domain (does not match user@foo.example.com)
+@example.com
+
+# a domain with subdomains (does match user@foo.example.com)
+example.com
+```
+
+There is also a special pattern `<UNI>`, which will whitelist an email address
+if it belongs to a known university (per
+[university-domains-list-api](https://github.com/Hipo/university-domains-list-api)).
--- app/s3downloader/whitelist.py
+++ app/s3downloader/whitelist.py
@@ -1,19 +1,111 @@
 import requests
+import os
 
 
 
-# https://github.com/Hipo/university-domains-list-api
-university_checker = 'http://universities.hipolabs.com/search?domain={domain}'
-
-def check_email(email):
-    try:
-        domain = email.split('@')[1]
-        r = requests.get(university_checker.format(domain=domain))
-        result = r.json()
-        if result:
-            return True
-    except Exception:
-        logger.exception("Error in university checker")
-    return False
 
 
+
+class WhitelistEntry:
+    @classmethod
+    def parse(cls, text):
+        if text == "<UNI>":
+            return UniversityEntry()
+        return PatternEntry(text)
+
+
+class UniversityEntry(WhitelistEntry):
+    # https://github.com/Hipo/university-domains-list-api
+    _university_checker = 'http://universities.hipolabs.com/search?domain={domain}'
+    specificity = 900
+
+    def __call__(self, email):
+        try:
+            domain = email.split('@')[1]
+            r = requests.get(self._university_checker.format(domain=domain))
+            result = r.json()
+            if result:
+                return True
+        except Exception:
+            logger.exception("Error in university checker")
+        return False
+
+
+class PatternEntry(WhitelistEntry):
+    def __init__(self, text):
+        self.text = text
+        black = text.startswith('!')
+        self.white = not black
+        if black:
+            text = text[1:]
+        self.name, at, domain = text.rpartition('@')
+        self.domain = domain.split('.')
+        self.exact = bool(at)
+        self.specificity = 1000 if at else len(self.domain) * 2
+        if black:
+            self.specificity += 1
+
+    def __call__(self, email):
+        name, at, domain = email.rpartition('@')
+        domain = domain.split('.')
+        if self.exact and self.domain != domain:
+            return False
+        if self.name and self.name != name:
+            return False
+        return domain[-len(self.domain):] == self.domain
+
+    def __repr__(self):
+        return self.text
+
+    def __eq__(self, other):
+        return self.specificity == other.specificity
+    def __ne__(self, other):
+        return self.specificity == other.specificity
+    def __gt__(self, other):
+        return self.specificity > other.specificity
+    def __lt__(self, other):
+        return self.specificity < other.specificity
+    def __ge__(self, other):
+        return self.specificity >= other.specificity
+    def __le__(self, other):
+        return self.specificity <= other.specificity
+
+
+# file format:
+#     name@host.domain - exact match of email
+#     @host.domain - exact match of host.domain
+#     subdomain.domain - match of subdomain.domain or more specific
+#     !entry - blacklist instead of whitelist
+#     <UNI> - match university list - see UniversityEntry
+#     # comments and blank lines are ignored
+#     # most specific match is applied
+class Whitelist:
+    def __init__(self, file):
+        self.file = file
+        self.whitelist = []
+        self.mtime = 0
+        self.load()
+
+    def load(self):
+        try:
+            mtime = os.path.getmtime(self.file)
+            if mtime > self.mtime:
+                with open(self.file) as r:
+                    self.whitelist = []
+                    self.mtime = mtime
+                    for line in r:
+                        line = line.strip()
+                        if not line or line.startswith('#'):
+                            continue
+                        entry = WhitelistEntry.parse(line)
+                        self.whitelist.append(entry)
+        except FileNotFoundError:
+            self.whitelist = []
+            self.mtime = 0
+
+    def __call__(self, email):
+        self.load()
+        best_match = max((entry for entry in self.whitelist if entry(email)), default=None)
+        if not best_match:
+            return False
+        return best_match.white
